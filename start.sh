bb5b3a53add3335acb2ec1948dbe6263c69c1dee
root <raitomm@gmail.com>
2015-08-27 20:47:45 +0000

    Fix start.sh

--- start.sh
+++ start.sh
@@ -9,10 +9,10 @@ fi
 
 /etc/init.d/ssh start
 
-cd /server/steamcmd && ./steamcmd.sh +login anonymous \
+cd /server/steamcmd && ./steamcmd.sh \
                  +@NoPromptForPassword 1 +login $STEAM_USERNAME $STEAM_PASSWORD \
 		 +force_install_dir /server/starbound \
-                 +app_update 367540 validate \
+                 +app_update 211820 validate \
                  +quit
 
 cd /server/starbound/linux64 && ./starbound_server
