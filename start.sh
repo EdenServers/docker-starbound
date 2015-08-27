1dfec8dad762dba9a770c8ffb895f7f69e622844
Thomas Geselle <raitomm@gmail.com>
2015-08-27 20:13:11 +0000

    Initial commit

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Setting up SFTP
+if ! id -u "$USERNAME" >/dev/null 2>&1; then
+    PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $PASSWORD)
+    useradd -d /server/starbound --shell /usr/bin/rssh --password $PASSWORD $USERNAME
+    chown -R $USERNAME:$USERNAME /server/starbound
+fi
+
+/etc/init.d/ssh start
+
+cd /server/steamcmd && ./steamcmd.sh +login anonymous \
+                 +@NoPromptForPassword 1 +login $STEAM_USERNAME $STEAM_PASSWORD \
+		 +force_install_dir /server/starbound \
+                 +app_update 367540 validate \
+                 +quit
+
+cd /server/starbound/linux64 && ./starbound_server
